56020da60e5e6082b44ba3d28b331055619bc982
Josh Baker <joshuabaker@users.noreply.github.com>
2016-09-21 13:18:26 +0100

    Generated plugin files

---
+++ rollbar/RollbarPlugin.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Rollbar plugin for Craft CMS
+ *
+ * Craft integration with error monitoring service Rollbar.
+ *
+ * @author    Joshua Baker
+ * @copyright Copyright (c) 2016 Joshua Baker
+ * @link      https://joshuabaker.com/
+ * @package   Rollbar
+ * @since     0.1.0
+ */
+
+namespace Craft;
+
+class RollbarPlugin extends BasePlugin
+{
+    /**
+     * @return mixed
+     */
+    public function init()
+    {
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+         return Craft::t('Rollbar');
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return Craft::t('Craft integration with error monitoring service Rollbar.');
+    }
+
+    /**
+     * @return string
+     */
+    public function getDocumentationUrl()
+    {
+        return 'https://github.com/craft-rollbar/rollbar/blob/master/README.md';
+    }
+
+    /**
+     * @return string
+     */
+    public function getReleaseFeedUrl()
+    {
+        return 'https://raw.githubusercontent.com/craft-rollbar/rollbar/master/releases.json';
+    }
+
+    /**
+     * @return string
+     */
+    public function getVersion()
+    {
+        return '0.1.0';
+    }
+
+    /**
+     * @return string
+     */
+    public function getSchemaVersion()
+    {
+        return '0.1.0';
+    }
+
+    /**
+     * @return string
+     */
+    public function getDeveloper()
+    {
+        return 'Joshua Baker';
+    }
+
+    /**
+     * @return string
+     */
+    public function getDeveloperUrl()
+    {
+        return 'https://joshuabaker.com/';
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasCpSection()
+    {
+        return false;
+    }
+
+    /**
+     */
+    public function onBeforeInstall()
+    {
+    }
+
+    /**
+     */
+    public function onAfterInstall()
+    {
+    }
+
+    /**
+     */
+    public function onBeforeUninstall()
+    {
+    }
+
+    /**
+     */
+    public function onAfterUninstall()
+    {
+    }
+}
